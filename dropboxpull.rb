890fe33f19ecff774db83af57814b57dc18f60fd
Ivan Kocienski <ivan.kocienski@gmail.com>
2012-01-08 13:02:45 +0000

    more hacking. proper gem required, some syntax SNAFUs fixed

--- dropboxpull.rb
+++ dropboxpull.rb
@@ -1,5 +1,5 @@
 
-require 'dropbox-sdk'
+require 'dropbox_sdk'
 require 'json'
 require 'open-uri'
 
@@ -10,6 +10,7 @@ require 'open-uri'
 APP_KEY = '6r39d4hnm3ngtyd'
 APP_SECRET = 'lhp4ez06xb1vxpi'
 
+ACCESS_TYPE = :app_folder
 
 
 
@@ -20,11 +21,12 @@ def login
 
   $session.get_request_token
 
-  authorize_url = @session.get_authorize_url
+  authorize_url = $session.get_authorize_url
 
   # dev hack work around
+  puts "authorizing"
   #page = open(authorize_url).read
-  #raise "Did not authorize" unless page ~= /Success/
+  #raise "Did not authorize" unless page =~ /Success/
 
 
   puts "Got a request token.  Your request token key is #{$session.request_token.key} and your token secret is #{$session.request_token.secret}"
@@ -36,7 +38,7 @@ def login
   # get the access token from the server. Its then stored in the session.
   $session.get_access_token
 
-  $client = DropboxClient.new(@session, ACCESS_TYPE)
+  $client = DropboxClient.new($session, ACCESS_TYPE)
 
 end
 
@@ -65,8 +67,23 @@ end
 # for each (PDF) file, extract text 
 def extract_text(filepath)
 
+  outfile = "#{filepath[1..filename.rindex('.')]}.txt"
+
   system( "pdftotext #{src_file} > #{outfile}" )
 
+  raise "pdf conversion failed" if $?
 end
 
+
+login
+
+ls_files.each do |meta|
+
+  next unless meta[:mime_type] == 'application/pdf'
+
+  save_file meta, get_file(meta)
+
+  extract_text "processing/#{File.basename(meta[:path])}"
+
+end
 
